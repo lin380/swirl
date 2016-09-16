c165b70e148f6a5ade07be80344d1ea7c7d629c8
Jerid Francom <francojc@MacBook-Pro.local>
2016-09-16 10:20:15 -0400

    Updated 'Packages and functions' lesson

---
+++ Packages and functions/outline.R
@@ -0,0 +1,10 @@
+
+# ABOUT -------------------------------------------------------------------
+
+# Outline for lesson
+
+
+# 1. Definition of functions
+# 2. Purpose of functions
+# 3. Structure of functions
+# 4. 
---
+++ Packages and functions/scripts/boring_function-correct.R
@@ -0,0 +1,3 @@
+boringFunction <- function(x) {
+  x
+}
